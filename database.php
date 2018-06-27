770fc9d268a1649f4b873da1d0a909cb3553802a
Tsumanchuk Liubava <karvit311@gmail.com>
2018-06-27 17:29:02 +0300

    database

--- database.php
+++ database.php
@@ -1,14 +1,8 @@
 <?php
-$host = '127.0.0.1';
-$db   = 'api';
-$user = 'root';
-$pass = 'Re_zinaidaromanova311888';
-$charset = 'utf8';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$opt = [
-    \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
-    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-    \PDO::ATTR_EMULATE_PREPARES   => false,
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
 ];
-$pdo = new \PDO($dsn, $user, $pass, $opt);
